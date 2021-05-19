e1632de291c7056a608cfaf57b41f3b36332d191
marlondck <15050029+marlondck@users.noreply.github.com>
2021-05-18 21:24:01 -0400

    mostrando a duração do audio

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import lottieWeb from 'https://cdn.skypack.dev/lottie-web';
 
 const playIconContainer = document.getElementById('play-icon');
-
+const audio = document.querySelector('audio');
+const durationContainer = document.getElementById('duration');
 
 let state = 'play';
 
+// Animarion
 const animation = lottieWeb.loadAnimation({
   container: playIconContainer,
   path: 'https://maxst.icons8.com/vue-static/landings/animated-icons/icons/pause/pause.json',
@@ -16,6 +18,36 @@ const animation = lottieWeb.loadAnimation({
 
 animation.goToAndStop(14, true);
 
+
+// Util para formatação do tempo
+const calculateTime = (secs) => {
+  const minutes = Math.floor(secs / 60);
+  const seconds = Math.floor(secs % 60);
+  const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+  return `${minutes}:${returnedSeconds}`;
+}
+
+//
+const displayDuration = () => {
+  durationContainer.textContent = calculateTime(audio.duration);
+}
+
+// Audio carregado
+audio.addEventListener('loadedmetadata', () => {
+  displayAudioDuration(audio.duration);
+});
+
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/readyState
+if (audio.readyState > 0) {
+  displayDuration();
+} else {
+  audio.addEventListener('loadedmetadata', () => {
+    displayDuration();
+  });
+}
+
+
+// Click no botão
 playIconContainer.addEventListener('click', () => {
   if(state === 'play') {
     animation.playSegments([14, 27], true);
